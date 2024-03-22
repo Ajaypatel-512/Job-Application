6b4373b252138df1bf4282a27e268432d5725577
Ajaypatel <Ajaypatelap1504@gmail.com>
2024-03-22 09:53:39 +0530

    Create and Get Job By Id

--- src/main/java/com/practise/FirstJobApp/job/JobController.java
+++ src/main/java/com/practise/FirstJobApp/job/JobController.java
@@ -3,6 +3,9 @@ package com.practise.FirstJobApp.job;
 import com.practise.FirstJobApp.job.entity.Job;
 import com.practise.FirstJobApp.job.service.JobService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
@@ -19,13 +22,24 @@ public class JobController {
     }
 
     @GetMapping("/jobs")
-    public List<Job> findAll(){
-        return jobService.findAll();
+    public ResponseEntity<List<Job>> findAll(){
+        return new ResponseEntity<>(jobService.findAll(),HttpStatus.OK);
     }
 
     @PostMapping("/jobs")
-    public String createJob(@RequestBody Job job){
+    public ResponseEntity<String> createJob(@RequestBody Job job){
         jobService.createJob(job);
-        return "Job Created Successfully";
+        return new ResponseEntity<>("Job Created Successfully",HttpStatus.CREATED);
     }
+
+    @GetMapping("/jobs/{id}")
+    public ResponseEntity<Job> getJobById(@PathVariable Long id){
+//        return jobService.findJobById(id) == null ? null : jobService.findJobById(id);
+        Job job = jobService.findJobById(id);
+        if (job == null){
+            return new ResponseEntity<>(job, HttpStatus.NOT_FOUND);
+        }
+        return new ResponseEntity<>(job, HttpStatus.OK);
+    }
+
 }
--- src/main/java/com/practise/FirstJobApp/job/service/impl/JobServiceImpl.java
+++ src/main/java/com/practise/FirstJobApp/job/service/impl/JobServiceImpl.java
@@ -22,4 +22,14 @@ public class JobServiceImpl implements JobService {
         job.setId(nextId++);
         jobs.add(job);
     }
+
+    @Override
+    public Job findJobById(Long id) {
+        for (Job job: jobs) {
+            if (job.getId().equals(id)){
+                return job;
+            }
+        }
+        return null;
+    }
 }
